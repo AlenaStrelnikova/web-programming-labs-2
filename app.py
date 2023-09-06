94a5cbeea2b36b6c392cfc317ff1e8c3e4dcf9b3
Алёна Стрельникова <soulhelen@yandex.ru>
2023-09-06 17:38:34 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <ol>
+            <li><a href="/lab1">Первая лабораторная</a></li>
+        </ol>
+        
+        <footer>
+            &copy; Алёна Стрельникова, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +40,12 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+
+        <div>Flask — фреймворк для создания веб-приложений на языке 
+        программирования Python, использующий набор инструментов Werkzeug,
+        а также шаблонизатор Jinja2. Относится к категории так называемых
+        микрофреймворков — минималистичных каркасов веб-приложений, 
+        cознательно предоставляющих лишь самые базовые возможности.</div>
 
         <footer>
             &copy; Алёна Стрельникова, ФБИ-14, 3 курс, 2023
