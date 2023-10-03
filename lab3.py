5237625b2cc32f6b13e94b193cb7c3ea8bb18471
Алена Стрельникова <soulhelen@yandex.ru>
2023-10-03 20:07:16 +0700

    Заказ напитка_недоработанный

--- lab3.py
+++ lab3.py
@@ -6,6 +6,7 @@ lab3 = Blueprint('lab3', __name__)
 def lab():
     return render_template('lab3.html')
 
+
 @lab3.route('/lab3/form1')
 def form1():
     errors = {}
@@ -19,3 +20,48 @@ def form1():
 
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    # order = ''
+    # drink = request.args.get('drink')
+
+    # if drink == 'coffee':
+    #     order = 'Кофе'
+    # elif drink == 'black-tea':
+    #     order = 'Черный чай'
+    # else:
+    #     order = 'Зеленый чай'
+
+    # if request.args.get('milk') == 'on':
+    #     order = 'с молоком'
+    # if request.args.get('sugar') == 'on':
+    #     order = 'с сахаром'
+
+    
+    return render_template('order.html', order=order)
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('pay.html', price = price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
