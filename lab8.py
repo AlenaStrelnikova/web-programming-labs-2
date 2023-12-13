30b96c3b142bbf39bd51b78aabc4d55828ddc807
Алена Стрельникова <soulhelen@yandex.ru>
2023-12-13 17:49:38 +0700

    Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -21,3 +21,10 @@ def get_course(course_num):
     if course_num < 0 or course_num >= len(courses):
         return "Error 404: Курс не найден", 404
     return courses[course_num]
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        return "Error 404: Курс не найден", 404
+    del courses[course_num]
+    return '', 204
