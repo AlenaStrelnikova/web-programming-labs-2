7a252e421857cfacc39e2647f88dd093a913a5a6
Алена Стрельникова <soulhelen@yandex.ru>
2023-12-19 16:10:27 +0700

    Обработчики ошибок 404 и 500

---
+++ lab9.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, abort
+
+lab9 = Blueprint ("lab9", __name__)
+
+
+@lab9.route('/lab9/')
+def main ():
+    return render_template ('lab9/index.html')
+
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/error404.html'), 404
+
+
+@lab9.app_errorhandler(500)
+def server_error(e):
+    return render_template('lab9/error500.html'), 500
+
+
+@lab9.route('/lab9/500')
+def error_500():
+    abort(500)
