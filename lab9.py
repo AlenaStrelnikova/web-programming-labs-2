77db28613715d022a86962eb2e874796d57ca8b9
Алена Стрельникова <soulhelen@yandex.ru>
2023-12-19 17:07:54 +0700

    Поздравительная открытка

--- lab9.py
+++ lab9.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort,  request
 
 lab9 = Blueprint ("lab9", __name__)
 
@@ -21,3 +21,21 @@ def server_error(e):
 @lab9.route('/lab9/500')
 def error_500():
     abort(500)
+
+
+@lab9.route('/lab9/card')
+def card ():
+    errors = {}
+
+    name1 = request.args.get('name1')
+    name2 = request.args.get('name2')
+    sex = request.args.get('sex')
+    happy = "Будь счастлив в Новом Году!" if sex == "man" else "Будь счастлива в Новом Году!"
+
+    if name1 == '':
+        errors['name'] = 'Заполните поле!'
+        
+    if name2 == '':
+        errors['name'] = 'Заполните поле!'
+
+    return render_template('lab9/card.html', name1=name1, name2=name2, sex=sex, happy=happy, errors=errors)
